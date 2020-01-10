80dea8dfc0d35f4954ab1c97a08e1fa1ed240a58
yukimakura <yukimakura86@gmail.com>
2020-01-10 12:53:51 +0900

    add ir

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,16 @@ use csv_import::csv_parse;
 fn main() {
     let mut datas: Vec<csv_parse::SCANDATA> = Vec::new();
     datas = csv_parse::read_sensor_data("sensor_data_600.csv".to_string()).unwrap();
+
+    // let v_ir = &probability_calc::probability_calc::calc_probabries_ir_with_time(datas).unwrap();
+    let v_lidar = &probability_calc::probability_calc::calc_probabries_lidar_with_time(datas).unwrap();
     
-    // for item in calc_probabries(datas).unwrap(){
-    //     println!("{:?},{:?}",item.element,item.probability);
-    // }
-    // probability_calc::probability_calc::plot(probability_calc::probability_calc::calc_probabries_lidar(datas,datas.len()).unwrap());
-    println!("{:?}",probability_calc::probability_calc::calc_probabries_lidar_with_time(datas).unwrap()[0]);
+    println!{"{:?}",v_lidar.len()}
+    probability_calc::probability_calc::plot(&v_lidar[14]);
+    for (num,item) in v_lidar.iter().enumerate() {
+        println!("{:?}",num);
+        println!("{:?}",item);
+    }
+
+    
 }
--- src/probability_calc.rs
+++ src/probability_calc.rs
@@ -12,8 +12,57 @@ pub struct PROBABILITY{
     pub probability :f64
 }
 
+pub fn calc_probabries_ir_with_time(input_vec : Vec<csv_parse::SCANDATA>) -> Result<Vec<Vec<PROBABILITY>>, Box<dyn Error>>{
+    let mut datas :Vec<Vec<csv_parse::SCANDATA>> = Vec::new();
+    let mut probs :Vec<Vec<PROBABILITY>> = Vec::new();
+
+    for n in 0..24{
+        datas.push(vec![csv_parse::SCANDATA{ date : -255, time : -255,lidar : -255, ir : -255}]);
+    }
+    
+    for item in input_vec {
+        let hour = (item.time - (item.time % 10000)) / 10000;
+        if(datas[hour as usize][0].ir == -255){
+            datas[hour as usize][0] = item;
+        }else{
+            datas[hour as usize].push(item);
+        }
+        
+    }
+    for item in &datas{
+        probs.push(calc_probabries_ir(item,item.len()).unwrap());
+    }
+
+    Ok(probs)
+}
+
+pub fn calc_probabries_ir(input_vec : &Vec<csv_parse::SCANDATA>,max_size : usize) -> Result<Vec<PROBABILITY>, Box<dyn Error>> {
+    let mut probs : Vec<PROBABILITY> = Vec::new();
+    let mut prob_element_check_flag : bool = false;
+    let mut p_buff : PROBABILITY;
+
+    for item in input_vec{
+        for mut p_item in &mut probs{
+            if p_item.element == item.ir {
+                p_item.probability = p_item.probability + 1.0;
+                prob_element_check_flag = true;
+            }
+        }
+        if prob_element_check_flag == false {
+            let p_buff  = PROBABILITY {element: item.ir , probability: 1.0};
+            probs.push(p_buff);
+        }
+        prob_element_check_flag = false;
+    }
+    probs.sort_by(|a, b| a.element.cmp(&b.element));
+    for item in &mut probs{
+        item.probability = item.probability / ( max_size as f64);
+    }
+
+    Ok(probs)
+}
+
 pub fn calc_probabries_lidar_with_time(input_vec : Vec<csv_parse::SCANDATA>) -> Result<Vec<Vec<PROBABILITY>>, Box<dyn Error>>{
-// pub fn calc_probabries_lidar_with_time(input_vec : Vec<csv_parse::SCANDATA>) -> Result<(), Box<dyn Error>>{
     let mut datas :Vec<Vec<csv_parse::SCANDATA>> = Vec::new();
     let mut probs :Vec<Vec<PROBABILITY>> = Vec::new();
 
@@ -63,7 +112,7 @@ pub fn calc_probabries_lidar(input_vec : &Vec<csv_parse::SCANDATA>,max_size : us
     Ok(probs)
 }
 
-pub fn plot(datas : Vec<PROBABILITY>) {
+pub fn plot(datas : &Vec<PROBABILITY>) {
     let mut fg = Figure::new();
     let mut x : Vec<i32> = Vec::new();
     let mut y : Vec<f64> = Vec::new();
